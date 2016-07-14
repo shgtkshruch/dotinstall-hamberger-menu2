448c2f7f17909c5f0fd24f8b2bc8cc7bdb76bcbb
shgtkshruch <shgtk.shruch@gmail.com>
2016-07-14 16:30:24 +0900

    Complete

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -1,1 +1,14 @@
+(() => {
+  'use strict';
 
+  const show = document.getElementById('js-show');
+  const hide = document.getElementById('js-hide');
+
+  show.addEventListener('click', function() {
+    document.body.classList.add('menu-open');
+  });
+
+  hide.addEventListener('click', function() {
+    document.body.classList.remove('menu-open');
+  });
+})();
